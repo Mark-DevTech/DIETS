eea937b38ae82777839a9e1012a6df8210b61311
mark033100 <marktumbaga31@gmail.com>
2025-01-31 16:35:46 +0800

    Initial Commit, Nuxt 3 + Sakai Template, Login Logic

---
+++ nuxt.config.ts
@@ -0,0 +1,52 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import Theme from '@primevue/themes/aura';
+
+export default defineNuxtConfig({
+    compatibilityDate: '2024-04-03',
+    devtools: { enabled: true },
+    modules: ['@primevue/nuxt-module', '@nuxtjs/tailwindcss'],
+    srcDir: 'src/',
+    css: [
+        '/assets/styles.scss',
+    ],
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {}
+        }
+    },
+    primevue: {
+        options: {
+            ripple: true,
+            theme: {
+                preset: Theme,
+                options: {
+                    darkModeSelector: '.app-dark'
+                }
+            }
+        }
+    },
+    plugins: ['~/plugins/primevue.js'],
+    app: {
+        head: {
+            charset: 'utf-8',
+            viewport: 'width=device-width, initial-scale=1',
+            title: 'DIETS 2.0'
+        },
+    },
+    vite: {
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    silenceDeprecations: ['legacy-js-api'],
+                },
+            }
+        }
+    },
+    runtimeConfig: {
+        AUTH_API: process.env.AUTHENTICATION_API,
+        public: {
+            AUTH_API: process.env.AUTHENTICATION_API || 'http://localhost:8000',
+        },
+    }
+})
---
+++ src/components/TopbarComponents/UserInformation.vue
@@ -0,0 +1,37 @@
+
+<script lang="ts" setup>
+  import { ref } from 'vue';
+  import { useAuth } from '~/composables/authentication/useAuth';
+
+  const userInfo = useCookie('authUser');
+
+  const clickedLogout = async () => {
+    const result = await useAuth().logout();
+    
+    if(result.status === 'success') {
+      console.log('Logout success');
+    }
+
+  }
+
+
+
+</script>
+
+
+
+<template>
+  <div class="w-full">
+    <p class="font-semibold p-0 m-0">{{ userInfo.firstname }} {{ userInfo.lastname }}</p>
+    <p style="font-style: italic" class="text-sm">{{ userInfo.postitle }}</p>
+    <div class="w-full flex justify-end">
+      <Button label="Logout" severity="danger" @click="clickedLogout" raised text />
+    </div>
+    
+  </div>
+</template>
+
+
+<style>
+
+</style>
